f4f8158a1a3c6032431802b1687d266d1a2978f6
sid-the-loser <69863204+sid-the-loser@users.noreply.github.com>
2020-11-10 17:44:18 +0530

    Update update error fix.py

--- update error fix.py
+++ update error fix.py
@@ -1,14 +1,14 @@
-import turtle
+import turtle # This imports the turtle module
 
-wn = turtle.Screen()
-wn.tracer(0)
+wn = turtle.Screen() # We declared "wn" as a turtle window
 
-player = turtle.Turtle()
+player = turtle.Turtle() # This declares variable "Player" as a turtle object
 
 while True: # You can use other stuff other than True
     try: # Make sure to put all your looping code in this
-        wn.update()
-        player.right(30)
+        wn.update() # This updates the window and every thing in it
+        player.right(30) # This just turns the turtle object aka "Player" to the right side in 30 degrees
+        
     except Exception as e: # If the update brings up an error; this stops the error from showing up at all
         print(e) # This prints out the error
         break # This just breaks the loop
@@ -21,3 +21,5 @@ I did that because people are not perfect and they could make errors in the code
 will just close and you will bhe left all confuced on what happened, that is why I printed out the error. This line (line 13) is only used for debugging purposes and 
 after you are done with you are done with your code (if you done the whole coding and you are going to publish the code to executable or anything else you can get rid of
 that print statement"""
+
+# Sorry if there was any spelling errors
